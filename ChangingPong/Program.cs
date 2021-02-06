b46e36b301b009b6bd773b01779491691cda6747
AzureJames <azurethegreat@gmail.com>
2021-02-05 20:08:18 -0500

    Add project files.

---
+++ ChangingPong/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BreakOut
+{
+    class Program
+    {
+        static object lockObject = new object();
+        static int ballPositionX = 0;
+        static int ballPositionY = 0;
+        static int ballDirectionX = -1;
+        static int ballDirectionY = 1;
+        static int firstPlayerPosition = 0;
+        static int point = 0;
+
+        static void SetBallAtTheMiddleOfTheGameField()
+        {
+            ballPositionX = Console.WindowWidth / 2;
+            ballPositionY = Console.WindowHeight / 2;
+        }
+
+        static void RemoveScrollBars()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+        }
+
+        static void Init()
+        {
+            point = 0;
+           
+           
+            SetBallAtTheMiddleOfTheGameField();
+           
+           
+        }
+
+        private static void MoveBall()
+        {
+            ballPositionX += ballDirectionX;
+            ballPositionY += ballDirectionY;
+
+           
+        }
+
+        static void BallRight ()
+        {
+            ballDirectionX = 1;
+            ballDirectionY = 0;
+        }
+
+        static void BallLeft()
+        {
+            ballDirectionX = -1;
+            ballDirectionY = 0;
+        }
+
+        static void BallUp()
+        {
+            ballDirectionX = 0;
+            ballDirectionY = 1;
+        }
+
+        static void BallDown()
+        {
+            ballDirectionX = 0;
+            ballDirectionY = -1;
+        }
+
+
+
+        static void PrintAtPosition(int x, int y, char symbol)
+        {
+            lock (lockObject)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(symbol);
+            }
+        }
+
+        static Queue<int[]> ballPath = new Queue<int[]>();
+
+        static void RemoveBall()
+        {
+            int[] a;
+            while (ballPath.Count > 0)
+            {
+                a = ballPath.Dequeue();
+                PrintAtPosition(a[0], a[1], ' ');
+            }
+        }
+
+        static void DrawBall()
+        {
+            PrintAtPosition(ballPositionX, ballPositionY, '@');
+            ballPath.Enqueue(new int[] { ballPositionX, ballPositionY });
+        }
+
+        static void CursorAtTheStart()
+        {
+            Console.SetCursorPosition(0, 0);
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 50;
+            Console.BufferWidth = 50;
+            RemoveScrollBars();
+            Init();
+            
+            int lineCounter = 0;
+            Console.WriteLine(" ,    ,  .  , .    ,   <||   .,  , ,  .  , ,      ");
+            Console.WriteLine(" ,  , .  ,  .  , .      ||    .,  , ,  .  ,    . ,");
+            while (lineCounter < 13)
+
+            {
+                Console.WriteLine(" ,  ,   ,  .  , .   .  ,  .  .,  , ,  .  ,  .  , ,");
+                Console.WriteLine(". ,  , .  ,  .  , .   .     .,  , ,  .  ,  .  .   ");
+                lineCounter++;
+            }
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+
+                    if (Console.KeyAvailable)
+                    {
+                        RemoveBall();
+                        ConsoleKeyInfo keyInfo = Console.ReadKey();
+                        if (keyInfo.Key == ConsoleKey.RightArrow)
+                        {
+                            point++;
+                            BallRight();
+                            MoveBall();
+                            
+                        }
+                        if (keyInfo.Key == ConsoleKey.LeftArrow)
+                        {
+                            point++;
+                            BallLeft();
+                            MoveBall();
+                            
+                        }
+                        if (keyInfo.Key == ConsoleKey.UpArrow)
+                        {
+                            point++;
+                            BallDown();
+                            MoveBall();
+                            
+                        }
+                        if (keyInfo.Key == ConsoleKey.DownArrow)
+                        {
+                            point++;
+                            BallUp();
+                            MoveBall();
+                            
+                        }
+                        DrawBall();
+
+                        if (ballPositionY== 1 && ballPositionX >= 24 && ballPositionX <=25)
+                        {
+                            Console.Write($"\n\n\n\nscore:{point}");
+                        }
+
+                    }
+                }
+            }).Start();
+
+            CursorAtTheStart();
+
+            while (true)
+            {
+                //RemoveBall();
+                //MoveBall();
+
+                DrawBall();
+                Thread.Sleep(120);
+            }
+        }
+    }
+}
---
+++ LeeCarvalloPuttingChallenge/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Threading;
+
+namespace LeeCarvalloPuttingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool putter = false;
+            bool iron = false;
+            bool driver = false;
+            int strokes = 0;
+            string userInput;
+            int x = 0;
+
+            string[] holeOne = { "0000000000", "00SS.P.000", "00SS...000", "000///0000", "00////00SS", "00////00SS", "00///000SS", "000*000000" };
+
+            Console.WriteLine("WELCOME TO LEE CARVALLO'S PUTTING CHALLENGE!");
+            Thread.Sleep(2900);
+
+            //while (x < 4)
+            //{
+                //first hole
+                
+
+                /*
+                 *            #
+                0000000000    0
+                00SS.P.000    1
+                00SS...000    2
+                000///0000    3
+                00////00WW    4
+                00////00WW    5
+                00///000WW    6
+                000*000000    7
+                */
+
+                foreach (var item in holeOne) //draw course
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine($"\nSTROKES:{strokes}\n MAP:   / fairway   0 rough  . green   * places ball has been   P  flag  S  sand");
+                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+
+                Console.WriteLine("What club are you using next?  i=iron d=driver p=putter");
+                userInput = Console.ReadLine();  //input club
+                Thread.Sleep(500);
+                Console.WriteLine("\n\n");
+
+
+                switch (userInput) //assign which club is being used based on userInput
+                {
+                    case "i":
+                        iron = true;
+                        putter = false;
+                        driver = true;
+                        break;
+                    case "p":
+                        putter = true;
+                        iron = false;
+                        driver = false;
+                        break;
+                    case "d":
+                        driver = true;
+                        putter = false;
+                        iron = false;
+                        break;
+                    default:
+                        iron = true;
+                        driver = false;
+                        putter = false;
+                        break;
+                }
+
+
+                if (putter)  //read which club is being used and update the map
+                {
+                    holeOne[6] = "00/*/000SS";
+                }
+                else if (iron)
+                {
+                    holeOne[4] = "00//*/00SS";
+                }
+                else //driver
+                {
+                    holeOne[2] = "00SS*..000";
+                }
+
+
+                //end of first hole
+                strokes++;
+                x++;
+            //}
+
+
+
+
+
+
+            //experiment second stroke, FIRST STROKE WAS IRON
+            /*
+                     *            #
+                    0000000000    0
+                    00SS.P.000    1
+                    00SS...000    2
+                    000///0000    3
+                    00//^/00SS    4
+                    00////00WW    5
+                    00///000WW    6
+                    000^000000    7
+                    */
+
+
+            foreach (var item in holeOne) //draw course
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"\nSTROKES:{strokes}\n MAP:   / fairway   0 rough  . green   * places ball has been   P  flag  S  sand");
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+            Console.WriteLine("What club are you using next?  i=iron d=driver p=putter");
+            userInput = Console.ReadLine();  //input club
+            Thread.Sleep(500);
+            Console.WriteLine("\n\n");
+
+
+            switch (userInput) //assign which club is being used based on userInput
+            {
+                case "i":
+                    iron = true;
+                    putter = false;
+                    driver = true;
+                    break;
+                case "p":
+                    putter = true;
+                    iron = false;
+                    driver = false;
+                    break;
+                case "d":
+                    driver = true;
+                    putter = false;
+                    iron = false;
+                    break;
+                default:
+                    iron = true;
+                    driver = false;
+                    putter = false;
+                    break;
+            }
+
+            
+            if (putter)  //read which club is being used and update the map
+            {
+                holeOne[3] = "000/*/0000"; //done
+            }
+            
+            
+            if (iron)
+            {
+                holeOne[1] = "00SS.P*000";
+            }
+            else //driver
+            {
+                holeOne[0] = "0000000000";
+            }
+
+            strokes++;
+            Thread.Sleep(3000);
+
+        }
+    }
+}
+    
+
